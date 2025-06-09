f4f32afa67a5866ae1b78a58b435d86f00b27da1
zzurlo <52360724+zzurlo@users.noreply.github.com>
2025-06-09 02:18:00 +0000

    updating .gitignore

--- FieldForge.Api/obj/Debug/net8.0/FieldForge.Api.AssemblyInfo.cs
+++ FieldForge.Api/obj/Debug/net8.0/FieldForge.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FieldForge.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b809193670c87c7937059c336fdf599c8c386c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baab0a8b08e9128053a7dd598a4e541f2dd5af79")]
 [assembly: System.Reflection.AssemblyProductAttribute("FieldForge.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FieldForge.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
